169c782b5a84f99699a0c3a30ee66df6246e4e54
gtungatkar <gauravstt@gmail.com>
2010-10-18 22:04:52 -0400

    Basic admin sign_in working. Redirects to admin/index for now

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -11,4 +11,11 @@ class Admin
   key :username
   key :password
   key :password_confirmation
+
+ def self.find_for_authentication(conditions={})
+    if conditions[:username] =~ /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i # email regex
+      conditions[:email] = conditions.delete(:username)
+    end
+    super
+  end
 end
