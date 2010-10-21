860ee62bb2e651e0bedf173aadd0d56d9772e683
sagar <sagar@sagar-laptop.(none)>
2010-10-21 09:51:34 -0400

    added a link 'Invite a friend!'on the admin home page which redirects to admin/new

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,12 @@
 class AdminController < ApplicationController
   #Code here
+  before_filter :authenticate_admin!
   def index
     
   end
-
+  def new
+    
+  end
   def update_signups
     #do nothing for now
     flash[:notice] = "signups opened"
